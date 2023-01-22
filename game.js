cef29784aa99e21218de32de00807f3191e217f7
Diana-Kaba <kabadiana327@gmail.com>
2023-01-22 11:43:50 +0200

    Second commit

--- game.js
+++ game.js
@@ -1,11 +1,15 @@
+alert(
+  "Правила гри: одне натискання на неїстівне – програш; 10 натискань на їстівне – виграш. Успіхів!"
+);
+
 let timer; // таймер
-let i; // номер строки массива
-let j; // номер столбца массива
+let i; // номер рядка масиву
+let j; // номер стовпця масиву
 let number = 0; // номер картинки
-let count = 0; // начальное значение счета
-let interval = 1000;
+let count = 0; // початкове значення рахунку
+let interval = 1000; // початкове значення швидкісті
 let createButton = document.getElementById("create");
-createButton.addEventListener("click", create);
+createButton.addEventListener("click", create); // додаємо подію на натискання
 let thingsArr = [
   [
     "./images/children.png",
@@ -22,72 +26,69 @@ let thingsArr = [
 ];
 
 let field = document.getElementById("field"); // поле
-let msg = document.getElementById("msg");
+let msg = document.getElementById("msg"); // повідомлення
 
 function getRandomInt(n) {
-  // случайное число
+  // випадкове число
   return Math.floor(Math.random() * n);
 }
 
 function create() {
-  i = getRandomInt(2); // номер строки массива
-  j = getRandomInt(3); // номер столбца массива
-  let left = getRandomInt(550); // левый отступ
-  let top = getRandomInt(350); // верхний отступ
-  let id = "thing" + number; // создаем id по номеру
-  let thing = document.createElement("img"); // Создаем элемент img
-  thing.id = id; // Добавляем id='thing0' и т.д.
+  i = getRandomInt(2); // номер рядка масиву
+  j = getRandomInt(3); // номер стовпця масиву
+  let left = getRandomInt(550); // лівий відступ
+  let top = getRandomInt(350); // верхній відступ
+  let id = "thing" + number; // створюємо id за номером
+  let thing = document.createElement("img"); // створюємо елемент img
+  thing.id = id; // додаємо id='thing0' і т.д.
   //thing.setAttribute('id', id);
-  thing.src = thingsArr[i][j]; // Добавляем src='images/...' из массива.
+  thing.src = thingsArr[i][j]; // додаємо src='images/...' з масиву.
   //thing.setAttribute('src', thingsArr[i][j]);
 
-  thing.setAttribute("onclick", "check(" + id + ")"); // Добавляем обработку события click
+  thing.setAttribute("onclick", "check(" + id + ")"); // додаємо обробку події click
 
-  thing.style.left = left + "px"; // Устанавливаем левый отступ
-  thing.style.top = top + "px"; // Устанавливаем верхний отступ
-  if (i == 0) thing.className = "noneatable"; // Добавляем class='noneatable'
-  else thing.className = "eatable"; // Добавляем class='eatable'
+  thing.style.left = left + "px"; // встановлюємо лівий відступ
+  thing.style.top = top + "px"; // встановлюємо верхній відступп
+  if (i == 0) thing.className = "noneatable"; // додаємо class='noneatable'
+  else thing.className = "eatable"; // додаємо class='eatable'
 
-  field.appendChild(thing); // Встраиваем элемент на поле
+  field.appendChild(thing); // вбудовуємо елемент на полі
   removeThing(id);
   number++;
-  // if (number >= 20) {
-  //   stop();
-  //   score.innerHTML = count + " Game over!";
-  // }
 }
 
 function removeThing(id) {
   let item = document.getElementById(id);
   setTimeout(function () {
     field.removeChild(item);
-  }, interval); //удаляем через секунду
+  }, interval); // видаляємо через секунду
 }
 
 function check(thing) {
-  // let thing = document.getElementById(id);
   if (thing.className == "eatable") {
     speed();
-    count++; //увеличиваем счет
-  } else count--; //уменьшаем счет
-  // msg.innerHTML = count;
+    count++; // збільшуємо рахунок
+  } else {
+    count--;
+  } // зменшуємо рахунок
   if (thing.className == "noneatable") {
-    stop();
-    msg.innerHTML = " Game over!";
+    stop(); // завершуємо гру, бо натиснув на неїстівне
+    msg.innerHTML = " На жаль, гра завершена!";
   } else if (thing.className == "eatable" && count == 10) {
-    stop();
-    msg.innerHTML = " You win!";
+    stop(); // завершуємо гру, бо 10 раз натиснув на їстівне
+    msg.innerHTML = " Ти виграв, вітаю!";
   }
 }
 
-timer = setInterval(create, interval); // рисовать каждые 10мс
+timer = setInterval(create, interval); // малювати кожні 10 мс
 
 function stop() {
-  clearInterval(timer); // завершить анимацию
+  clearInterval(timer); // завершити анімацію
 }
 
 function speed() {
-  interval -= 30;
-  clearInterval(timer);
-  timer = setInterval(create, interval);
+  // фунція для збільшення швидкості
+  interval -= 30; // встановили новий час
+  clearInterval(timer); // очищаємо старий інтервал
+  timer = setInterval(create, interval); // задаємо новий
 }
